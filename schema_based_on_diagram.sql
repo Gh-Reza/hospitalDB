99fcddf536f4efafb0ad6d2c03d42bc58fd0883c
Reza Merzaie <ghrezam97@gmail.com>
2023-08-22 23:47:00 +0430

    Create table medical history

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -4,3 +4,14 @@ CREATE TABLE patients (
 	date_of_birth DATE
 );
 	
+CREATE TABLE medical_history (
+	id SERIAL PRIMARY KEY,
+	admitted_at TIMESTAMP,
+	patient_id INT,
+	status VARCHAR(50),
+	CONSTRAINT fk_patient_id FOREIGN KEY (patient_id) REFERENCES patients(id)
+);
+
+CREATE INDEX patient_id_index ON medical_history(patient_id);
+	
+
